1b9c795a26b37c775351516d5f392b0f98630def
Goktug Cakmakli <86517256+Goktugg0@users.noreply.github.com>
2025-10-25 22:38:15 +0200

    Update primitivity_check to use Poly type
    Refactor primitivity_check function to use Poly type for parameters and improve comments.

--- FiniteFieldArithmetic/primitivityCheck.py
+++ FiniteFieldArithmetic/primitivityCheck.py
@@ -1,24 +1,31 @@
-from utils import get_prime_factors
+from utils import Poly, get_prime_factors
 from .finiteFieldMultiplication import finite_field_multiply
 
-
-def primitivity_check(f, integer_modulus: int, polynomial_modulus) -> bool:
+# Check if a polynomial is primitive in a finite field
+def primitivity_check(f: Poly, integer_modulus: int, polynomial_modulus: Poly) -> bool:
+    # A polynomial f is primitive if its order is equal to the order of the multiplicative group of the finite field
     if f == [0] or f == [] or f == None:
         return False
-
+    
     p = len(polynomial_modulus)
-    order = integer_modulus ** (p - 1) - 1
+    # The order of the multiplicative group of the finite field GF(p^n) is p^n - 1
+    order = integer_modulus**(p - 1) - 1
     primeFactors = get_prime_factors(order)
+    # Check the condition for primitivity
     for i in primeFactors:
         power = order // i
         temp = f[:]
+        # Compute f^power mod polynomial_modulus
         while power > 0:
+            # power is even
             if power % 2 == 0:
                 temp = finite_field_multiply(temp, temp, polynomial_modulus, integer_modulus)
                 power //= 2
+            # power is odd
             else:
                 temp = finite_field_multiply(temp, f, polynomial_modulus, integer_modulus)
                 power -= 1
+        # If f^power mod polynomial_modulus == 1, then f is not primitive
         if temp == [1]:
             return False
     return True
